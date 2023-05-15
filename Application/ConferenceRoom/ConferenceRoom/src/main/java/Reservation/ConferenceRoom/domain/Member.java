2625764e7f63f4f261449894d55f3e8c39cd1558
JH <ajh8920@naver.com>
2023-05-15 23:35:32 +0900

    DB table name Change
    user TO member

--- Application/ConferenceRoom/ConferenceRoom/src/main/java/Reservation/ConferenceRoom/domain/Member.java
+++ Application/ConferenceRoom/ConferenceRoom/src/main/java/Reservation/ConferenceRoom/domain/Member.java
@@ -7,7 +7,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity
-public class User extends BaseEntity {
+public class Member extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
--- Application/ConferenceRoom/ConferenceRoom/src/main/java/Reservation/ConferenceRoom/domain/Reservation.java
+++ Application/ConferenceRoom/ConferenceRoom/src/main/java/Reservation/ConferenceRoom/domain/Reservation.java
@@ -10,8 +10,8 @@ public class Reservation extends BaseEntity {
     private Long id;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "user_id", nullable = false)
-    private User user;
+    @JoinColumn(name = "member_id", nullable = false)
+    private Member member;
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "room_id", nullable = false)
     private Room room;
