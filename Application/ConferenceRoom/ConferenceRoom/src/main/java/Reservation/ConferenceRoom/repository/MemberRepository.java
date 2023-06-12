4ff6704c7092f90663db0dcd4c33008725b8e336
JH <ajh8920@naver.com>
2023-06-12 22:26:50 +0900

    home2
    home2

--- Application/ConferenceRoom/ConferenceRoom/src/main/java/Reservation/ConferenceRoom/repository/MemberRepository.java
+++ Application/ConferenceRoom/ConferenceRoom/src/main/java/Reservation/ConferenceRoom/repository/MemberRepository.java
@@ -1,10 +1,8 @@
-public interface MemberRepository extends JpaRepository<Member,Long> {
 package Reservation.ConferenceRoom.repository;
 
 import Reservation.ConferenceRoom.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-
-public interface MemberRepository extends JpaRepository<Long,Member> {
+public interface MemberRepository extends JpaRepository<Member,Long> {
     Member save(Member member);
 }
